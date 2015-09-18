efe8a8a767760f6476b1edee559bd52634407eb9
Derwei Chan <derwei@pivotal.io>
2015-09-18 09:46:24 -0400

    Updates install script to install some prerequisites

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+set +e
+go version
+GO_INSTALLED=$?
+set -e
+if [ $GO_INSTALLED -ne 0 ] || [ -z $GOPATH ] ; then
+  echo "You need to install Go Language and set the GOPATH first."
+  exit 1
+fi
+
+# need to install the cli before installing autopilot
+# if [ -]
+mkdir -p $GOPATH/src/github.com/cloudfoundry
+pushd $GOPATH/src/github.com/cloudfoundry/
+rm -rf cli
+git clone https://github.com/cloudfoundry/cli.git
+popd
+
 echo 'Building new `autopilot` binary...'
 go install
 
 echo 'Installing the plugin...'
-cf uninstall-plugin Autopilot
+cf plugins | grep autopilot
+AUTOPILOT_INSTALLED=$?
+
+if [ $AUTOPILOT_INSTALLED -ne 0 ]; then
+  cf uninstall-plugin Autopilot
+fi
+
 cf install-plugin $GOPATH/bin/autopilot
